ed6b17dfacb7e101cf2a89de0e8f7d59720658d1
lucasmr92 <lucas_mr92@hotmail.com>
2018-02-27 23:14:34 -0300

    Implementado mapeamento de objeto para objeto

---
+++ src/ObjectConverter/AutoConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ObjectConverter
+{
+    public class AutoConverter
+    {
+        /// <summary>
+        /// Método faz a conversão do objeto passado por parâmetro de origem para o objeto de destino.
+        /// </summary>
+        /// <typeparam name="TOrigem"></typeparam>
+        /// <typeparam name="TDestino"></typeparam>
+        /// <param name="objetoOrigem"></param>
+        /// <returns>retorna objeto de destino</returns>
+        public static TDestino ToConvert<TOrigem, TDestino>(TOrigem objetoOrigem)
+        {
+            var attributesObjectOrigem = typeof(TOrigem).GetProperties();
+            var attributesObjectDestino = typeof(TDestino).GetProperties();
+            var objetoDestino = Activator.CreateInstance(typeof(TDestino));
+
+            foreach (var attributeOrigem in attributesObjectOrigem)
+            {
+                foreach (var attributeDestino in attributesObjectDestino)
+                {
+                    if (attributeOrigem.Name == attributeDestino.Name && attributeOrigem.GetType() == attributeDestino.GetType())
+                    {
+                        var propertyInfo = objetoDestino.GetType().GetProperty(attributeDestino.Name);
+                        if (propertyInfo != null)
+                        {
+                            if (attributeOrigem.PropertyType.IsAssignableFrom(attributeDestino.PropertyType))
+                            {
+                                propertyInfo.SetValue(objetoDestino, attributeOrigem.GetValue(objetoOrigem, null), null);
+                                break;
+                            }
+                            Type tipoOrigem = attributeOrigem.PropertyType.GetGenericArguments()[0];
+                            Type tipoDestino = attributeDestino.PropertyType.GetGenericArguments()[0];
+
+                            if (tipoOrigem.IsClass)
+                            {
+                                var lista = (ICollection)attributeOrigem.GetValue(objetoOrigem, null);
+
+                                var tipoEspecifico = typeof(List<>).MakeGenericType(new Type[] { tipoDestino });
+                                var novaLista = (IList)Activator.CreateInstance(tipoEspecifico);
+
+                                foreach (var objeto in lista)
+                                {
+                                    var methodInfo = typeof(AutoConverter).GetMethod("ToConvert");
+                                    if (methodInfo != null)
+                                    {
+                                        var metodo = methodInfo.MakeGenericMethod(tipoOrigem, tipoDestino);
+                                        var objetoConvertido = metodo.Invoke(null, new object[] { objeto });
+                                        novaLista.Add(objetoConvertido);
+                                    }
+                                }
+                                propertyInfo.SetValue(objetoDestino, novaLista, null);
+                            }
+                        }
+                    }
+                }
+            }
+            return (TDestino)objetoDestino;
+        }
+    }
+}
---
+++ src/Sample/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using ObjectConverter;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listaCarros = new List<Objeto1>();
+            listaCarros.Add(new Objeto1(1, "Objeto1"));
+            listaCarros.Add(new Objeto1(2, "Objeto1"));
+            var classeOrigem = new ClasseOrigem("Teste", "XPTO", listaCarros);
+            var objetoConvertido = AutoConverter.ToConvert<ClasseOrigem, ClasseDestino>(classeOrigem);
+        }
+    }
+
+
+    class ClasseOrigem
+    {
+        public ClasseOrigem(string nome, string endereco, IList<Objeto1> carros)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Carros = carros;
+        }
+
+        public string Nome { get; set; }
+
+        public string Endereco { get; set; }
+
+        public IList<Objeto1> Carros { get; set; }
+    }
+
+    class ClasseDestino
+    {
+        public ClasseDestino(string nome, string endereco, string telefone)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Telefone = telefone;
+        }
+
+        public ClasseDestino() { }
+
+        public string Nome { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Telefone { get; set; }
+
+        public IList<Objeto2> Carros { get; set; }
+    }
+
+    class Objeto1
+    {
+        public Objeto1(long id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+        public long Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+
+    class Objeto2
+    {
+        public long Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
